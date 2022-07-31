0a6ec118c97672d6efae0994c03bf32045384686
lison <740394554@qq.com>
2022-07-31 22:14:30 +0800

    feat: 修改项目为Monorepo管理

---
+++ play/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>测试代码</div>
+</template>
--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import Tree from '../src/tree';
-
-createApp(App).use(Tree).mount('#app');
